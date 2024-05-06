99a2522d850bd0485e3cef0480370f89ac7de800
JayMGurav <jaymgruav@gmail.com>
2024-05-06 11:05:23 +0530

    swapping implementations

--- src/main/java/com/example/demo/services/Impl/HindiRedPrinter.java
+++ src/main/java/com/example/demo/services/Impl/HindiRedPrinter.java
@@ -3,11 +3,11 @@ package com.example.demo.services.Impl;
 import com.example.demo.services.RedPrinter;
 import org.springframework.stereotype.Component;
 
-
-public class EnglishRedPrinter implements RedPrinter {
+@Component
+public class HindiRedPrinter implements RedPrinter {
 
     @Override
     public String print() {
-        return "red";
+        return "laal";
     }
 }
