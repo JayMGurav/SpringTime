336a023a1d782d4622eed780ccdadae11a9e02d2
JayMGurav <jaymgruav@gmail.com>
2024-05-07 11:33:26 +0530

    connecting to H2 db

--- src/main/java/com/example/demo/PizzaApplication.java
+++ src/main/java/com/example/demo/PizzaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
+
 @Log
 public class PizzaApplication implements CommandLineRunner {
 
